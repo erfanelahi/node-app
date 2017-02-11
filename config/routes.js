f9fd3c14e0da4505cbd21353f47a9396601ba1ed
erfan elahi <erfan elahi>
2017-02-11 19:42:10 +0600

    update

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,12 @@
 var router = require("express").Router();
-
+var bodyParser = require('body-parser');
+//
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
+//
 router.get("/", function (request, response) {
     response.render("index.ejs", { message: "Hello World." });
 });
-
+//
 module.exports = router;
--- server.js
+++ server.js
@@ -1,27 +1,22 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+var engine = require("ejs-locals");
 var mongoose = require('mongoose');
 var mongodbUri = require('mongodb-uri');
-var engine = require("ejs-locals");
+//
 var app = express();
-
-var urlencodedParser = bodyParser.urlencoded({
-    extended: false
-});
-var port = process.env.PORT || 3000;
-
 app.engine("ejs", engine);
-
+//
 app.use("/", function (req, res, next) {
     console.log("Request URL : " + req.url);
     next();
 });
-
+//
 app.use("/content", express.static(__dirname + "/assets"));
-
+//
 var routes = require("./config/routes");
 app.use(routes);
-
+//
+var port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
